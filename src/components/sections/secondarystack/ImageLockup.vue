626d0045f2baa11dd54cc5d23099ca7c9b691926
Jay Clark <jay@jayeclark.dev>
2022-03-07 10:49:26 -0400

    Add secondary stack
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

---
+++ src/components/sections/secondarystack/ImageLockup.vue
@@ -0,0 +1,64 @@
+<script lang="js">
+export default {
+  name: 'ImageLockup',
+  props: ['ribbonHeight', 'adjustedWidth', 'imgHeight', 'tech'],
+  data() {
+    return {
+      ratings: [0, 1, 2, 3],
+      titles: [
+        "I've worked on or contributed to an OSS project using this, but am still a novice",
+        "I've used this in one of my own projects or extensively in an OSS one, and am actively learning more",
+        "I have used this extensively and have successfully debugged and/or done troubleshooting with more complex situations.",
+        "I consider proficient in this relative to my overall career experience. I can hit the ground running in this language or environment.",
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <div
+    :style="{
+      height: `${ribbonHeight}px`,
+      width: `${adjustedWidth}px`,
+      padding: '0px 10px',
+    }"
+  >
+    <div
+      class="image-div"
+      :style="{
+        height: `${imgHeight}px`,
+        minHeight: `${imgHeight}px`,
+        maxHeight: `${imgHeight}`,
+      }"
+    >
+      <div class="image-alt-name" :style="{ display: tech.url ? 'none' : '' }">
+        {{ tech.name }}
+      </div>
+      <img
+        :src="tech.url"
+        :alt="tech.name"
+        :height="tech.dimension && tech.dimension === 'height' ? '100' : ''"
+        :width="!tech.dimension || tech.dimension === 'width' ? '100' : ''"
+        :style="{ display: tech.url ? '' : 'none' }"
+      />
+    </div>
+    <div
+      class="ratings-div"
+      style="display: flex; flex-wrap: nowrap; justify-content: center"
+    >
+      <template v-for="(rating, idx) in ratings" :key="idx">
+        <div
+          :style="{
+            margin: `${25 - rating * 5}px 2px 5px 2px`,
+            border: `1px solid #666`,
+            width: `10px`,
+            height: `${10 + rating * 5}px`,
+            backgroundColor: tech.competence >= rating + 1 ? 'green' : '#ccc',
+          }"
+          :title="titles[rating]"
+        ></div>
+      </template>
+    </div>
+  </div>
+</template>
---
+++ src/components/sections/secondarystack/SecondaryStackSection.vue
@@ -0,0 +1,267 @@
+<script lang="js">
+import { stack } from "@/scripts/technologies";
+import { nextTick } from "vue";
+import ImageLockup from "./ImageLockup.vue";
+export default {
+    name: "SecondaryStackSection",
+    data() {
+        return {
+            technologies: stack,
+            startingCard: 0,
+            cardWidth: 170,
+            imgHeight: 100,
+            ribbonHeight: 135,
+            arrowWidth: 20,
+            scrollInProgress: false,
+            ratings: [0, 1, 2, 3],
+        };
+    },
+    computed: {
+        cardsToDisplay() {
+            const availableWidth = window.innerWidth * 0.82 - this.arrowWidth;
+            return Math.floor(availableWidth / this.cardWidth);
+        },
+        adjustedWidth() {
+            const availableWidth = window.innerWidth * 0.82 - this.arrowWidth;
+            const cardsToDisplay = Math.floor(availableWidth / this.cardWidth);
+            return Math.floor(availableWidth / (cardsToDisplay * 10)) * 10;
+        },
+        techSlice() {
+            const start = this.startingCard;
+            const num = this.cardsToDisplay;
+            const len = this.technologies.length;
+            if (start + num > len) {
+                return [...this.technologies.slice(start, len), ...this.technologies.slice(0, start + num - len)];
+            }
+            return this.technologies.slice(start, start + num);
+        },
+        beforeSlice() {
+            const technologies = [];
+            this.technologies.forEach(t => technologies.push({ ...t }));
+            const num = this.cardsToDisplay;
+            let start = this.startingCard;
+            let newStart = start - num;
+            const len = technologies.length;
+            if (newStart < 0) {
+                newStart += len;
+            }
+            if (newStart + num > len) {
+                return [...technologies.slice(newStart, len), ...technologies.slice(0, newStart + num - len)];
+            }
+            return technologies.slice(newStart, newStart + num);
+        },
+        afterSlice() {
+            const technologies = [];
+            this.technologies.forEach(t => technologies.push({ ...t }));
+            const num = this.cardsToDisplay;
+            let start = this.startingCard;
+            let newStart = start + num;
+            const len = technologies.length;
+            if (newStart > len) {
+                newStart -= len;
+            }
+            if (newStart + num > len) {
+                return [...this.technologies.slice(newStart, len), ...this.technologies.slice(0, newStart + num - len)];
+            }
+            return this.technologies.slice(newStart, newStart + num);
+        }
+    },
+    methods: {
+        setUpScroll(dir) {
+            this.scrollInProgress = true;
+            this.$refs.before.style.transition = "left 0.8s ease";
+            this.$refs.current.style.transition = "left 0.8s ease";
+            this.$refs.after.style.transition = "left 0.8s ease";
+            const newBefore = dir === "left" ? "0px" : this.$refs.before.style.left;
+            const newCurrent = dir === "left" ? `${this.cardsToDisplay * this.adjustedWidth}px` : `${-1 * this.cardsToDisplay * this.adjustedWidth}px`;
+            const newAfter = dir === "left" ? this.$refs.after.style.left : "0px";
+            return { newBefore, newCurrent, newAfter };
+        },
+        scroll(dir) {
+            if (this.scrollInProgress)
+                return;
+            const positions = this.setUpScroll(dir);
+            // Set new positions
+            setTimeout(() => {
+                this.$refs.before.style.left = positions.newBefore;
+                this.$refs.current.style.left = positions.newCurrent;
+                this.$refs.after.style.left = positions.newAfter;
+            }, 20);
+            // Set new state and reset transitions
+            setTimeout(() => {
+                const [start, num, len] = [this.startingCard, this.cardsToDisplay, this.technologies.length];
+                this.startingCard = dir === "left" ? (start - num < 0 ? start - num + len : start - num) : (start + num > len ? start + num - len : start + num);
+                this.$refs.before.style.transition = "";
+                this.$refs.current.style.transition = "";
+                this.$refs.after.style.transition = "";
+                this.scrollInProgress = false;
+                nextTick(() => {
+                    this.$refs.before.style.left = `${-1 * this.adjustedWidth * this.cardsToDisplay}px`;
+                    this.$refs.current.style.left = "0px";
+                    this.$refs.after.style.left = `${this.adjustedWidth * this.cardsToDisplay}px`;
+                });
+            }, 800);
+            // Reset positions
+            //setTimeout(() => {
+            //  this.$refs.before.style.left = `${-1 * this.adjustedWidth * this.cardsToDisplay}px`;
+            //  this.$refs.current.style.left = "0px";
+            //  this.$refs.after.style.left = `${this.adjustedWidth * this.cardsToDisplay}px`
+            //}, 810)
+        },
+    },
+    components: { ImageLockup }
+}
+</script>
+<template>
+  <div class="technologies" style="width: 100%">
+    <hr />
+    <span class="centered"><h6>Additional languages & skills</h6></span>
+
+    <div class="ribbon-div">
+      <div
+        class="left-arrow"
+        :style="{ width: `${arrowWidth}px`, height: `${ribbonHeight}px` }"
+        @click="scroll('left')"
+      >
+        <img src="@/assets/images/chevron-left.svg" />
+      </div>
+      <div
+        class="cards-container"
+        :style="{
+          width: `${adjustedWidth * cardsToDisplay}px`,
+          height: `${ribbonHeight}px`,
+        }"
+      >
+        <div
+          class="cards-div tech-cards"
+          ref="before"
+          :style="{
+            width: `${adjustedWidth * cardsToDisplay}px`,
+            height: `${ribbonHeight}px`,
+            left: `${-1 * adjustedWidth * cardsToDisplay}px`,
+          }"
+        >
+          <ImageLockup
+            v-for="(tech, index) in beforeSlice"
+            :key="index"
+            :ribbonHeight="ribbonHeight"
+            :adjustedWidth="adjustedWidth"
+            :imgHeight="imgHeight"
+            :tech="{ ...tech }"
+          ></ImageLockup>
+        </div>
+        <div
+          class="cards-div tech-cards"
+          ref="current"
+          :style="{
+            width: `${adjustedWidth * cardsToDisplay}px`,
+          }"
+        >
+          <ImageLockup
+            v-for="(tech, index) in techSlice"
+            :key="index"
+            :ribbonHeight="ribbonHeight"
+            :adjustedWidth="adjustedWidth"
+            :imgHeight="imgHeight"
+            :tech="tech"
+          ></ImageLockup>
+        </div>
+        <div
+          class="cards-div tech-cards"
+          ref="after"
+          :style="{
+            width: `${adjustedWidth * cardsToDisplay}px`,
+            left: `${adjustedWidth * cardsToDisplay}px`,
+          }"
+        >
+          <ImageLockup
+            v-for="(tech, index) in afterSlice"
+            :key="index"
+            :ribbonHeight="ribbonHeight"
+            :adjustedWidth="adjustedWidth"
+            :imgHeight="imgHeight"
+            :tech="tech"
+          ></ImageLockup>
+        </div>
+      </div>
+      <div
+        class="right-arrow"
+        :style="{ width: `${arrowWidth}px`, height: `${ribbonHeight}px` }"
+        @click="scroll('right')"
+      >
+        <img src="@/assets/images/chevron-right.svg" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+hr {
+  width: 30%;
+  border-bottom: 1px solid #ccc;
+  margin: 40px auto 20px auto !important;
+}
+.ribbon-div {
+  display: flex;
+  width: 100%;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+}
+.cards-container {
+  display: flex;
+  overflow: hidden;
+  position: relative;
+}
+.image-div {
+  margin: auto 0px;
+  overflow: hidden;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+}
+.image-alt-name {
+  border: 1 px solid #666;
+  padding: 10px;
+  border-radius: 4px;
+  text-align: center;
+  margin: auto 0px;
+}
+.left-arrow,
+.right-arrow {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #ddd;
+  cursor: pointer;
+}
+.cards-div {
+  position: absolute;
+  display: flex;
+  left: 0px;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+  padding: 0px 10px;
+}
+.logo {
+  width: 180px;
+  flex-shrink: 0;
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+  padding: 5px;
+}
+.logo-container {
+  display: flex;
+  flex-wrap: wrap;
+  width: 250px;
+  flex-shrink: 1;
+  padding: 10px 0px;
+}
+.logo-container {
+  flex-grow: 1;
+  max-width: 550px;
+}
+</style>
